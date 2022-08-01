b8f105b1650156d74db2977c52fbf12a6f5b8306
Zohaib <zohaibmanzoor1700@gmail.com>
2022-07-31 18:05:02 -0700

    added notes

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-//server
 require('dotenv').config();
-// require cors, express
+
 const express = require('express');
 const cors = require('cors');
+// -------Routers-------
 const userRouter = require('./routes/userRouter')
 
 const app = express();
 
-// Middleware 
+// -------Middleware-------
 app.use(express.json());
 app.use(cors());
 
-// Routes 
-app.use(userRouter)
+// -------Routes-------
+app.use(userRouter);
 
 const PORT = 4000;
 app.listen(PORT, () => {
@@ -20,10 +20,11 @@ app.listen(PORT, () => {
 });
 
 /*
-npx knex migrate:make table_name
+Create new table: npx knex migrate:make table_name
+migrate: npx knex migrate:latest
+rollback: npx knex migrate:rollback
 
-seeds 
-npx knex seed:make 
-01_users_seeds
+Create new seed: npx knex seed:make 01_users_seeds
+run seeds: npx knex seed:run
 */
 
