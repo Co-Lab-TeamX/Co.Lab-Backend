e8351dcb3aff1dde7c682976f3f99f8419a9b8df
Zohaib <zohaibmanzoor1700@gmail.com>
2022-08-03 18:53:02 -0700

    authFinished

--- routes/interestedRouter.js
+++ routes/interestedRouter.js
@@ -1,9 +1,9 @@
 const interestedRouter = require("express").Router();
 const { getAllInterested, addInterested, deleteInterested } = require("../controllers/interestedController");
 
-interestedRouter.get('/posts/:post_id/interested', getAllInterested)
+interestedRouter.get('/posts/:post_id/interested', getAllInterested);
 
-interestedRouter.post('/posts/:post_id/interested', addInterested)
+interestedRouter.post('/posts/:post_id/interested', addInterested);
 
 interestedRouter.delete('/posts/:post_id/interested', deleteInterested);
 
