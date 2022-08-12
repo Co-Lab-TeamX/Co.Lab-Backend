5acd3b2296aceb978729a675ced1791e51bcbe73
Zohaib <zohaibmanzoor1700@gmail.com>
2022-08-11 19:46:52 -0700

    added condition column to table and seeds

--- migrations/20220731230156_posts.js
+++ migrations/20220731230156_posts.js
@@ -14,6 +14,7 @@ exports.up = function (knex) {
         table.string('location');
         table.string('category');
         table.integer('quantity');
+        table.string('condition');
         table.integer('weight');
         table.integer('length');
         table.integer('height');
