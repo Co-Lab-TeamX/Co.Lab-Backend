4c1a693283b9a598c95ce52abeecf27af4fd6ce1
Zohaib <zohaibmanzoor1700@gmail.com>
2022-08-15 21:17:09 -0700

    added chat seeds:

--- seeds/03_chats_seeds.js
+++ seeds/03_chats_seeds.js
@@ -4,6 +4,9 @@
  */
 exports.seed = async function (knex) {
   await knex('chats').insert([
-    { message_body: 'hey is this still available?', receiver_id: 2, sender_id: 1 },
+    { message_body: 'hey is this still available?', receiver_id: 1, sender_id: 2 },
+    { message_body: 'hey is this still available?', receiver_id: 3, sender_id: 2 },
+    { message_body: 'hey is this still available?', receiver_id: 3, sender_id: 2 },
+    { message_body: 'hey is this still available?', receiver_id: 4, sender_id: 2 },
   ]);
 };
