3db64b82f56fc8fa5a6e2d189e22c4922c433048
jacobnathanson <jacobnath93@gmail.com>
2022-08-18 16:56:19 -0400

    added netlify url to cors config

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://freeupnyc.netlify.app/'],
     methods: ["GET", "POST"]
   }
 });
@@ -37,7 +37,7 @@ io.on("connection", socket => {
     socket.broadcast.emit("receive_message");
   });
 
-  socket.on("disconnect",() => {
+  socket.on("disconnect", () => {
     console.log("user disconnected", socket.id);
   });
 });
