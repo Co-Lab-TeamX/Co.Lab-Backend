ef4344bc76c0bc24428b4a619009f611b093bca9
Zohaib <zohaibmanzoor1700@gmail.com>
2022-09-01 20:53:16 -0700

    updatedSeeds and added 3 more

--- seeds/04_comments_seeds.js
+++ seeds/04_comments_seeds.js
@@ -4,9 +4,14 @@
  */
 exports.seed = async function (knex) {
   await knex('comments').insert([
-    { post_id: 1, user_id: 2, comment_body: "is this still available?" },
-    { post_id: 1, user_id: 2, comment_body: "I'd like to buy it" },
-    { post_id: 2, user_id: 1, comment_body: "I'd like this chair" },
-    { post_id: 2, user_id: 1, comment_body: "is it in good condition?" },
+    { post_id: 1, user_id: 4, comment_body: "Is this still available?" },
+    { post_id: 1, user_id: 2, comment_body: "This is nice! my aunt also has one similar to this" },
+    { post_id: 2, user_id: 3, comment_body: "Is this still available?" },
+    { post_id: 3, user_id: 4, comment_body: "Is this still available?" },
+    { post_id: 4, user_id: 3, comment_body: "Is this still available?" },
+    { post_id: 5, user_id: 4, comment_body: "Is this still available?" },
+    { post_id: 6, user_id: 2, comment_body: "Is this still available?" },
+    { post_id: 7, user_id: 1, comment_body: "Is this still available?" },
+    { post_id: 8, user_id: 1, comment_body: "Is this still available?" },
   ]);
 };
